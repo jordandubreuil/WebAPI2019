f5e7cca7b8557784bc8eadf73de68023430c1f98
Andrew <vurtua7@hotmail.com>
2019-01-31 15:05:11 -0500

    Class on Thursday Jan 31

--- GameListApp/app.js
+++ GameListApp/app.js
@@ -34,18 +34,26 @@ app.use(bodyParser.json());
 //Route to index.html
 router.get('/', function(req,res){
     //res.sendFile(path.join(__dirname+'/index.html'));
-    var title = "Welcome to the GameApp Page";
+    //var title = "Welcome to the GameApp Page";
 
-    res.render('index', {
-        title:title
-    });
+    res.render('index');
 });
 
-app.get('/getdata', function(req, res){
-    console.log("request made from fetch");
+//Route to entries
+router.get('/entries', function(req,res){
+   res.render('gameentries/addgame');
+});
+
+//Route to login
+router.get('/login', function(req,res){
+    res.render('login');
+ });
+
+app.get('/', function(req, res){
+    //console.log("request made from fetch");
     Entry.find({})
     .then(function(entries){
-        res.send({
+        res.render('index', {
             entries:entries
         });
     });
@@ -55,7 +63,7 @@ app.get('/getdata', function(req, res){
 
 
 //post from form on index.html
-app.post('/', function(req,res){
+app.post('/addgame', function(req,res){
     console.log(req.body);
     var newEntry = {
         title:req.body.title,
@@ -65,6 +73,15 @@ app.post('/', function(req,res){
     .save()
     .then(function(entry){
         res.redirect('/')
+    });
+});
+
+//Delete Game Entry
+app.post('/:id', function(req,res){
+    Entry.remove({_id:req.params.id})
+    .then(function(){
+      //  req.flash("game removed");
+        res.redirect('/');
     });
 });
 
